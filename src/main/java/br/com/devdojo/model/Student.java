a21103a4f4e106668872bdddeb0fb1a4639866e0
Diego Ocko Martins <dmartins143@gmail.com>
2021-01-02 17:22:39 -0300

    Backup Data until class 21

---
+++ src/main/java/br/com/devdojo/model/Student.java
@@ -0,0 +1,39 @@
+package br.com.devdojo.model;
+
+import javax.persistence.Entity;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+@Entity
+public class Student extends AbstractEntity {
+    @NotEmpty
+    private String name;
+
+    @NotEmpty
+    @Email
+    private String email;
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
